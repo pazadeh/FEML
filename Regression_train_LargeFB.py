ff3fe2826d174f13293d6454826e1da9f6ef5d0a
Pouyan Asgharzadeh <38374357+pazadeh@users.noreply.github.com>
2020-09-01 16:22:50 +0200

    Update and rename GBR_Largedef_FB_model_finder_02.py to Regression_train_LargeFB.py

--- Regression_train_LargeFB.py
+++ Regression_train_LargeFB.py
@@ -43,18 +43,9 @@ for i in range(0,300):
     print(i)
     first_time = True
        		
-       		# Create a dataframe with the four feature variables
-       	# Create two new dataframes, one with the training rows, one with the test rows
-       	#if len(test)>12:
-       	# Show the number of observations for the test and training dataframes
-       	 
-       	# Create a list of the feature column's names2
     X_train, X_test, y_train, y_test = train_test_split(df[features], df[Mechanical_param],
                                                         test_size=0.10, random_state=i, 
-                                                        stratify=df['Class'])       	# train['species'] contains the actual species names. Before we can use it,
-       	# we need to convert each species name into a digit. So, in this case there
-       	# are three species, which have been coded as 0, 1, or 2.
-    
+                                                        stratify=df['Class'])       
 
     model = GradientBoostingRegressor()
     param_grid = {
@@ -108,7 +99,7 @@ def save_obj(obj, name ):
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
         
 save_obj(df_models,'GBR_models_90p_d20_FB')
-# Root Mean Squared Error
+
 
 
 
